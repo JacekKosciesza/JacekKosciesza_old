09ebd04ec0a470038da94755059222056b07b7a9
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-11-30 17:00:00 +0100

    Picture, button, robots.txt, humans.txt

--- elements/blog/jk-blog.js
+++ elements/blog/jk-blog.js
@@ -14,18 +14,32 @@ customElements.define('jk-blog', class extends HTMLElement {
         const tmpl = doc.querySelector('#jk-blog-tmpl');
         this._root = this.attachShadow({mode: 'open'});
         this._root.appendChild(tmpl.content.cloneNode(true));
+
+        this._playButton = this._root.querySelector('#play'); 
     }
 
     connectedCallback() {
-
+      this.addEventListeners();
     }
 
     disconnectedCallback() {
-
+      this.removeEventListeners();
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
 
+    }
+
+    addEventListeners() {
+        this._playButton.addEventListener('click', this.onPlayButtonClicked);
+    }
+
+    removeEventListeners() {
+        this._playButton.removeEventListener('click', this.onPlayButtonClicked);
+    }
+
+    onPlayButtonClicked() {
+        console.log('Play button clicked');
     }
 });
 
