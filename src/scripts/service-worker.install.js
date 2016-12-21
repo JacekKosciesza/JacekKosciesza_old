40399a4420a3dfa99dce13884556bd7d9c72e436
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-12-21 12:21:28 +0100

    Service worker

--- src/scripts/service-worker.install.js
+++ src/scripts/service-worker.install.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', _ => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        navigator.serviceWorker.register('/service-worker.js', {scope: '/'}).then(registration => {
             console.info('Service Worker registration successful with scope: ', registration.scope);
         }).catch(err => {
             console.info('Service Worker registration failed: ', err);
--- src/service-worker.js
+++ src/service-worker.js
@@ -4,22 +4,17 @@ const NAME = 'JacekKosciesza';
 const VERSION = '0.1';
 const CACHE_NAME = `${NAME}-v${VERSION}`;
 
-var urlsToCache = [
+var ASSETS = [
     '/',
     '/styles/all.css'
 ];
 
-self.oninstall = evt => {
-    evt.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => {
-            //console.debug('SW (install): Opened cache, adding all URLs to cache');
-            return cache.addAll(urlsToCache);
-        })
-    );
+self.oninstall = evt => evt.waitUntil(async function () {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(ASSETS);
 
-    self.skipWaiting();
-}
+    return self.skipWaiting();
+}());
 
 self.onactivate = evt => {
     evt.waitUntil(
@@ -42,7 +37,7 @@ self.onactivate = evt => {
         })
     );
 
-    self.clients.claim();
+    self.clients.claim(); // take care of all opened tabs
 }
 
 self.onfetch = evt => {
@@ -58,4 +53,39 @@ self.onfetch = evt => {
             return fetch(evt.request);
         })
     );
+}
+
+// https://www.youtube.com/watch?v=X8EQSy-ajo4&index=21&list=PLNYkxOF6rcIBTs2KPy1E6tIYaWoFcG3uj
+function staleWhileRevalidate(evt) {
+    const fetchedVersion = fetch(evt.request);
+    const fetchedCopy = fetchedVersion.then(response => response.clone());
+    const cachedVersion = caches.match(evt.request);
+
+    evt.respondWith(async function () {
+        try {
+            const response = await Promise.race([
+                fetchedVersion.catch(_ => cachedVersion),
+                cachedVersion
+            ]);
+            if (!response) {
+                return await fetchedVersion;
+            }
+            return response;
+        } catch (_) {
+            return new Response(null, {
+                status: 404
+            });
+        }
+    }());
+
+    evt.waitUntil(async function () {
+        try {
+            const response = await fetchedCopy;
+            const cache = await caches.open(CACHE_NAME);
+
+            return cache.put(event.request, response);
+        } catch(_) {
+            // eat errors
+        }
+    }());
 }
