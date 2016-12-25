0b7dd4dee838137daee1aa73b79776fdac599d8d
Jacek Kościesza <jacek.kosciesza@gmail.com>
2016-12-25 12:37:18 +0100

    Service worker

---
+++ src/sw.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const NAME = 'JacekKosciesza';
+const VERSION = '0.2.0';
+const CACHE_NAME = `${NAME}-v${VERSION}`;
+
+var ASSETS = [
+    '/'
+];
+
+self.oninstall = evt => evt.waitUntil(async function () {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(ASSETS);
+
+    return self.skipWaiting();
+}());
+
+self.onactivate = evt => {
+    evt.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName.indexOf(NAME) === -1) {
+                        //console.debug(`SW (activate): found cache '${cacheName}', but it's not matching our cache name '${NAME}'`);
+                        return null;
+                    }
+
+                    if (cacheName !== CACHE_NAME) {
+                        //console.debug(`SW (activate): found old cache '${cacheName}' - deleting`);
+                        return caches.delete(cacheName);
+                    }
+
+                    return null;
+                })
+            );
+        })
+    );
+
+    self.clients.claim(); // take care of all opened tabs
+}
+
+self.onfetch = evt => {
+    evt.respondWith(
+        caches.match(evt.request)
+        .then(response => {
+            if (response) {
+                //console.debug(`SW (fetch): Cache hit '${evt.request.url}`);
+                return response;
+            }
+
+            //console.debug(`SW (fetch): Cache miss '${evt.request.url}`);
+            return fetch(evt.request);
+        })
+    );
+}
