0a09c58bac7920e00205e6313da74a33bc9a0bbe
Jacek Kościesza <jacek.kosciesza@gmail.com>
2017-01-03 08:25:06 +0100

    Templatizer in framework

--- src/framework/jk.js
+++ src/framework/jk.js
@@ -1,12 +1,21 @@
 'use strict';
 
-let JK = (function() {
+let JK = (function () {
     let jk = {};
 
     jk.Element = class extends HTMLElement {
         constructor() {
             super();
-            console.log('JK Element constructor');
+            const doc = document.currentScript.ownerDocument;
+            const tmpl = doc.querySelector(`#${this.constructor.is}`);
+            const shadowRoot = this.attachShadow({
+                mode: 'open'
+            });
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+        }
+
+        connectedCallback() {
+            console.log(`${this.constructor.is} connectedCallback`);
         }
     }
 
