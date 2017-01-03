31b13d2dbc0b6a88e57da61cc203b8fa50fdf937
Jacek Kościesza <jacek.kosciesza@gmail.com>
2017-01-03 11:20:21 +0100

    Attribute to property

--- src/framework/jk.js
+++ src/framework/jk.js
@@ -15,7 +15,27 @@ let JK = (function () {
         }
 
         connectedCallback() {
-            console.log(`${this.constructor.is} connectedCallback`);
+            console.log(`JK.Element(${this.constructor.is}) connectedCallback`);
+        }
+
+        attributeChangedCallback(attrName, oldVal, newVal) {
+            console.log(`JK.Element(${this.constructor.is}) attrName=${attrName}, oldVal=${oldVal}, newVal=${newVal}`);        
+            this[attrName] = this._setProperty(attrName, newVal);
+        }
+
+        disconnectedCallback() {
+            console.log(`JK.Element(${this.constructor.is}) disconnectedCallback`);
+        }
+
+        _setProperty(attrName, value) {
+            let type = this.constructor.config.properties[attrName];
+            switch (type) {
+                case 'Object':
+                    return JSON.parse(value);
+                    break;
+                default:
+                    return value;
+            }
         }
     }
 
