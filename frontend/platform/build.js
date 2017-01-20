a0bd75b1055ab8ab5dec68990ddcc111678d2b86
Jacek Kościesza <jacek.kosciesza@gmail.com>
2017-01-20 23:08:56 +0100

    Build: copy with hash postfix

--- frontend/platform/build.js
+++ frontend/platform/build.js
@@ -7,16 +7,22 @@ let hashmark = require('hashmark');
 let glob = require('glob');
 let chokidar = require('chokidar');
 let multimatch = require('multimatch');
+let crypto = require('crypto');
+const md5File = require('md5-file/promise');
+const path = require('path');
 
 let info = chalk.blue;
 let error = chalk.bold.red;
 
 console.log(info(`Building ${packageJson.name} v${packageJson.version}\n`));
 
-let config = [
-    { glob: 'src/scripts/*.js', tasks: [ 'uglyfy',  'hashmark', 'copy' ] }
-]
-let options = { watch: false };
+let config = [{
+    glob: 'src/scripts/*.js',
+    tasks: ['uglyfy', 'hashmark', 'copy']
+}]
+let options = {
+    watch: false
+};
 
 class Build {
     constructor(config, options) {
@@ -42,11 +48,23 @@ class Build {
     }
 
     watch() {
-        chokidar.watch('src', {ignored: /(^|[\/\\])\../}).on('change', (path) => {
+        chokidar.watch('src', {
+            ignored: /(^|[\/\\])\../
+        }).on('change', (changedPath) => {
             for (let entry of this.config) {
-                let files = multimatch([path.replace('/', '\\')], entry.glob)
+                let files = multimatch([changedPath.replace('/', '\\')], entry.glob)
                 if (files && files.length) {
-                    console.log(`${info(files)} -> ${chalk.green(entry.glob)}`);
+                    for (let file of files) {
+                        console.log(`${info(file)} -> ${chalk.green(entry.glob)}`);
+                        this.md5sum(file, 8).then(md5sum => {
+                            let parsedPath = path.parse(file)
+                            let hashedFilename = `${parsedPath.dir.replace('src', 'build')}\\${parsedPath.name}.${md5sum}${parsedPath.ext}`
+                            console.log(hashedFilename);
+                            return hashedFilename;
+                        }).then(hashedFilename => {
+                            fs.createReadStream(file).pipe(fs.createWriteStream(hashedFilename));
+                        });
+                    }
                 }
             }
         });
@@ -57,18 +75,22 @@ class Build {
         copy('src/*.txt', '.tmp', (err, files) => {
             if (err) throw err;
             // `files` is an array of the files that were copied
-            console.log(files); 
+            console.log(files);
         });
+    }
+
+    md5sum(filePath, length) {
+        return md5File(filePath).then(hash => {
+            return length ? hash.slice(0, length) : hash;
+        })
     }
 }
 
 process.argv.forEach((val, index, array) => {
-  if (val === 'watch') {
-      options.watch = true;
-  }
+    if (val === 'watch') {
+        options.watch = true;
+    }
 });
 
 let build = new Build(config, options);
 build.run();
-
-
